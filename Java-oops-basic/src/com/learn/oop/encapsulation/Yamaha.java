31400425070d22625dddfdaf299b0f13eee66ba0
unknown <cogjava4350@iiht.tech>
2022-11-09 18:57:06 +0530

    encapsulation added

---
+++ Java-oops-basic/src/com/learn/oop/encapsulation/Yamaha.java
@@ -0,0 +1,22 @@
+package com.learn.oop.encapsulation;
+
+public class Yamaha {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	
+	Bike FZ = new Bike();
+	FZ.setName("Yamaha FZ");
+	FZ.setCC(150);
+	FZ.setColor("Blue");
+	FZ.setPrice(90000);
+	 System.out.println(FZ.getName()); 
+	 
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
